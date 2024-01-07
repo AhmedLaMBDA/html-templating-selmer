775597251f85a03292d25cdcb27c4b16b18e9e2a
AhmedLaMBDA <ahmedamermousa@gmail.com>
2024-01-07 18:08:47 +0200

    Initial commit wiht content

---
+++ src/html_templating_selmer/core.clj
@@ -0,0 +1,6 @@
+(ns html-templating-selmer.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/html_templating_selmer/core_test.clj
@@ -0,0 +1,7 @@
+(ns html-templating-selmer.core-test
+  (:require [clojure.test :refer :all]
+            [html-templating-selmer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
